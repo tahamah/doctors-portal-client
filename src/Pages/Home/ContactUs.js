83a3e5cee161808bbd1fdc97edabe91b09763725
unknown <taha.iu.bd@gmail.com>
2022-05-14 15:59:07 +0600

    done dona done

--- src/Pages/Home/ContactUs.js
+++ src/Pages/Home/ContactUs.js
@@ -7,7 +7,7 @@ const ContactUs = () => {
             style={{
                 background: `url(${appointment})`,
             }}
-            className="bg-primary px-10 py-14 "
+            className="bg-primary px-10 mt-28 py-14 "
         >
             <div className="text-center pb-14 text-white">
                 <p className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-accent to-secondary">
